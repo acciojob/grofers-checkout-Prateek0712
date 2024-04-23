bd96c8c7ad897603bc8c522be6f0b12192dc91b7
Prateek Jadhav <134202260+Prateek0712@users.noreply.github.com>
2024-04-23 12:46:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,23 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  let  priceArr=document.querySelectorAll('.price');
+  console.log(priceArr);
+	let sum=0;
+	for( let  p of priceArr)
+		{
+			console.log(p.innerText);
+            sum+=Number(p.innerText);
+		}
+    //console.log("sum "+sum);
+	let tr_js=document.createElement('tr');
+    let  cell =tr_js.insertCell();
+    cell.colSpan=2;
+    cell.innerText= "total price: "+sum;
+    let  table_js= document.querySelector("table");
+	table_js.appendChild(tr_js);
+    let tableChilds=table_js.children;
+    console.log(tableChilds);
 };
 
 getSumBtn.addEventListener("click", getSum);
